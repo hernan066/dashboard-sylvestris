30d6f66ea7fc8f92effb0649acde9d4abac67f71
hernan0066 <hernan0066@gmail.com>
2022-12-02 20:06:04 -0300

    Add  products list

---
+++ src/components/products/productsList/ProductsList.jsx
@@ -0,0 +1,136 @@
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MenuListProducts from "./ManuListProducts";
+
+const ListProducts = ({ products = [] }) => {
+  const [pageSize, setPageSize] = useState(10);
+  const [open, setOpen] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const navigate = useNavigate();
+  console.log(products)
+
+  const handleOpenMenu = (id, event) => {
+    setOpen(event.currentTarget);
+    setUserId(id);
+  };
+
+  const handleCloseMenu = () => {
+    setOpen(null);
+    setUserId(null);
+  };
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "Id",
+      width: 50,
+    },
+    {
+      field: "image",
+      headerName: "Imagen",
+      width: 100,
+      renderCell: (params) => (
+        <Avatar
+          src={`http://localhost:3000/images/products/${params.row.image}`}
+        />
+      ),
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: "nombre",
+      headerName: "Nombre",
+      flex: 1,
+    },
+
+    {
+      field: "sub_titulo",
+      headerName: "Subtitulo",
+      flex: 1,
+    },
+    {
+      field: "stock",
+      headerName: "Stock",
+    },
+
+    {
+      field: "descripcion_altura",
+      headerName: "Altura",
+      flex: 1,
+    },
+    {
+      field: "descripcion_maceta",
+      headerName: "Maceta",
+      flex: 1,
+    },
+    {
+      field: "agua",
+      headerName: "Agua",
+    },
+    {
+      field: "luz",
+      headerName: "Luz",
+    },
+
+    {
+      field: "accessLevel",
+      headerName: "Menu",
+      headerClassName: "super-app-theme--header",
+
+      renderCell: ({ row: { id } }) => (
+        <IconButton
+          size="large"
+          color="inherit"
+          onClick={(e) => handleOpenMenu(id, e)}
+        >
+          <MoreVertIcon />
+        </IconButton>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div className="responsive-table">
+        <div className="admin__products__header">
+          <h2>Lista de productos</h2>
+
+          <button
+            className="btn__crear"
+            onClick={() => navigate("/users/create")}
+          >
+            <i className="bx bx-message-square-add"></i> Agregar Producto
+          </button>
+        </div>
+
+        <div style={{ height: 665, width: "100%" }}>
+          <DataGrid
+            checkboxSelection
+            disableSelectionOnClick
+            components={{ Toolbar: GridToolbar }}
+            rows={products.map((product) => ({
+              ...product,
+              image: product.images[0]?.filename || ''
+            }))}
+            columns={columns}
+            getRowId={(row) => row.id}
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[10, 15, 20]}
+            pagination
+          />
+        </div>
+      </div>
+      <MenuListProducts
+        open={open}
+        handleCloseMenu={handleCloseMenu}
+        userId={userId}
+      />
+    </>
+  );
+};
+
+export default ListProducts;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+import apiRequest from "../api/apiRequest";
+import Error from "../components/error/Error";
 import Layout from "../components/layouts/Layout";
+import Loading from "../components/loading/Loading";
+import ListProducts from "../components/products/productsList/ProductsList";
 
-const ProductPage = () => {
+
+
+const ProductsPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getListProducts = async () => {
+      try {
+        const { data } = await apiRequest("/products");
+       
+        if (data) {
+          setProducts(data.data.products);
+          setLoading(false);
+        }
+      } catch (error) {
+        setError(error)
+        setLoading(false);
+      }
+    };
+    getListProducts();
+  }, []);
+
   return (
-
-  <Layout>
-   {/*  aca va el codigo */}
-  </Layout>
-  )
+    <Layout>
+      {loading && <Loading />}
+      {error && <Error />}
+     {products && <ListProducts products={products} />} 
+    </Layout>
+  );
 };
 
-export default ProductPage;
+export default ProductsPage;
