23342b1416d25c013e1b5b82583647a1565e4098
hernan0066 <hernan0066@gmail.com>
2022-12-04 23:10:17 -0300

    Add edit images products

--- src/components/products/productEdit/EditProduct.jsx
+++ src/components/products/productEdit/EditProduct.jsx
@@ -1,15 +1,15 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as Yup from "yup";
-
 import Swal from "sweetalert2";
 import apiRequest from "../../../api/apiRequest";
+import ImageUpload from "./ImageUpload";
 
 const ONLY_NUMBERS = /^[0-9+]*$|^NULL$/;
 const ONLY_LETTERS = /^[a-zA-ZÁÉÍÓÚáéíóúñÑ ]+$/;
 const NUMBERS_AND_LETTERS = /^[a-zA-Z0-9ÁÉÍÓÚáéíóúñÑ ]+$/;
-const NUMBERS_LETTERS_SYMBOLS = /^[a-zA-Z0-9ÁÉÍÓÚáéíóúñÑ,.-:°!¡º ]+$/;
+const NUMBERS_LETTERS_SYMBOLS = /^[a-zA-Z0-9ÁÉÍÓÚáéíóúñÑ,.-:°|¡ ]+$/;
 
 const SignupSchema = Yup.object().shape({
   nombre: Yup.string()
@@ -19,29 +19,58 @@ const SignupSchema = Yup.object().shape({
     .required("Requerido")
     .matches(ONLY_LETTERS, "Solo letras"),
   stock: Yup.number().positive("Solo números positivos").required("Requerido"),
-  descripcion: Yup.string().required("Requerido").matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
-  cuidados: Yup.string().required("Requerido").matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
+  descripcion: Yup.string()
+    .required("Requerido")
+    .matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
+  cuidados: Yup.string()
+    .required("Requerido")
+    .matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
   categoria: Yup.string().required("Requerido"),
   destacado: Yup.string().required("Requerido"),
-  descripcion_altura: Yup.string().required("Requerido").matches(NUMBERS_LETTERS_SYMBOLS,"Solo letras y números"),
-  descripcion_maceta: Yup.string().required("Requerido").matches(NUMBERS_LETTERS_SYMBOLS,"Solo letras y números"),
+  descripcion_altura: Yup.string()
+    .required("Requerido")
+    .matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
+  descripcion_maceta: Yup.string()
+    .required("Requerido")
+    .matches(NUMBERS_LETTERS_SYMBOLS, "Solo letras y números"),
   precio: Yup.number().positive("Solo números positivos").required("Requerido"),
-  agua: Yup.number().positive("Solo números positivos").required("Requerido"),
-  luz: Yup.number().positive("Solo números positivos").required("Requerido"),
+  //image1: Yup.string().required("Requerido"),
+  //image2: Yup.string().required("Requerido"),
 });
 
-const EditProduct = ({product}) => {
+const EditProduct = ({ product }) => {
+  const {id}= useParams()
   const navigate = useNavigate();
-  const {id}= useParams();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [errorUpload1, setErrorUpload1] = useState(null);
+  const [errorUpload2, setErrorUpload2] = useState(null);
+  const [formError, setFormError] = useState({
+    image1: null,
+    image2: null,
+  });
+
+  const [urlImage1, setUrlImage1] = useState(product.images[0]?.filename || "");
+  const [urlImage2, setUrlImage2] = useState(product.images[1]?.filename || "");
 
   const handleSubmit = async (values) => {
+    // valido errores
+    if (!urlImage1 || !urlImage2) {
+      return setFormError({
+        image1: urlImage1 ? null : "Requerido",
+        image2: urlImage2 ? null : "Requerido",
+      });
+    }
+
     setLoading(true);
-console.log(values)
+    values.image1 = urlImage1;
+    values.image2 = urlImage2;
+    console.log(values);
+
     try {
       const { data } = await apiRequest.put(`/products/${id}`, values);
-       if (data) {
+
+      if (data) {
         Swal.fire({
           position: "center",
           icon: "success",
@@ -50,15 +79,50 @@ console.log(values)
           timer: 2500,
         });
         navigate("/products");
-      } 
+      }
     } catch (error) {
       setError(
-        error.response?.data?.errors[0]?.msg || "Error: producto no editado"
+        error.response?.data?.errors[0]?.msg || "Error: usuario no creado"
       );
       console.log(error.response?.data?.errors[0].msg);
     }
   };
 
+  useEffect(() => {
+    setFormError({
+      image1: urlImage1 ? null : "Requerido",
+      image2: urlImage2 ? null : "Requerido",
+    });
+  }, [urlImage1, urlImage2]);
+
+  useEffect(() => {
+    setFormError({
+      image1: null,
+      image2: null,
+    });
+  }, []);
+
+  const handlerDelete = () => {
+   
+    Swal.fire({
+      title: "Deseas borrar este producto?",
+      text: "Este cambio no se puede revertir",
+      icon: "danger",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await apiRequest.delete(`/products/${product.id}`);
+          navigate('/products')
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  };
   return (
     <div className="admin__container">
       <section className="login__main__container admin__form__container">
@@ -75,8 +139,8 @@ console.log(values)
             descripcion_altura: product.descripcion_altura,
             descripcion_maceta: product.descripcion_maceta,
             precio: product.precio,
-            agua: product.agua,
-            luz: product.luz,
+            image1: urlImage1,
+            image2: urlImage2,
           }}
           validationSchema={SignupSchema}
           onSubmit={(values, { resetForm }) => {
@@ -140,9 +204,15 @@ console.log(values)
                     <option value="" disable>
                       Elige una opción
                     </option>
-                    <option value="1" selected={values.categoria == 1 && true}>Suculentas</option>
-                    <option value="2" selected={values.categoria == 2 && true}>Cactus</option>
-                    <option value="3" selected={values.categoria == 3 && true}>Plantas</option>
+                    <option value="1" selected={values.categoria === 1}>
+                      Suculentas
+                    </option>
+                    <option value="2" selected={values.categoria === 2}>
+                      Cactus
+                    </option>
+                    <option value="3" selected={values.categoria === 3 && true}>
+                      Plantas
+                    </option>
                   </Field>
                   <ErrorMessage
                     name="categoria"
@@ -231,38 +301,73 @@ console.log(values)
                 </div>
 
                 <div className="form__field">
-                  <label for="">Agua(*)</label>
-                  <Field type="number" name="agua" className="input__admin" />
+                  <label for="">Imagen 1(*)</label>
 
-                  <ErrorMessage
-                    name="agua"
-                    component="p"
-                    className="form__error"
-                  />
+                  <div style={{ display: "flex", alignItems: "flex-start" }}>
+                    <Field
+                      type="text"
+                      name="image1"
+                      className="input__admin"
+                      value={urlImage1}
+                    />
+
+                    <ImageUpload
+                      setErrorUpload={setErrorUpload1}
+                      setUrlImage={setUrlImage1}
+                    />
+                  </div>
+                  {formError.image1 && (
+                    <p className="form__error">{formError.image1}</p>
+                  )}
                 </div>
                 <div className="form__field">
-                  <label for="">Luz(*)</label>
-                  <Field type="number" name="luz" className="input__admin" />
+                  <label for="">Imagen 2(*)</label>
+                  <div style={{ display: "flex", alignItems: "flex-start" }}>
+                    <Field
+                      type="text"
+                      name="image2"
+                      className="input__admin"
+                      value={urlImage2}
+                    />
 
-                  <ErrorMessage
-                    name="luz"
-                    component="p"
-                    className="form__error"
-                  />
+                    <ImageUpload
+                      setErrorUpload={setErrorUpload2}
+                      setUrlImage={setUrlImage2}
+                    />
+                  </div>
+
+                  {formError.image2 && (
+                    <p className="form__error">{formError.image2}</p>
+                  )}
                 </div>
-
-                <button
-                  className={`btn-load ${loading ? "button--loading" : ""}`}
-                  type="submit"
-                  disabled={loading}
-                  style={{
-                    marginTop: "28px",
-                    padding: "9px",
-                    backgroundColor: "#1c6a6d",
-                  }}
-                >
-                  <span className="button__text">Enviar</span>
-                </button>
+                <div style={{ display: "flex", gap:'10px' }}>
+                  <button
+                    className={`btn-load ${loading ? "button--loading" : ""}`}
+                    type="submit"
+                    disabled={loading}
+                    style={{
+                      marginTop: "28px",
+                      padding: "9px",
+                      backgroundColor: "#1c6a6d",
+                    }}
+                  >
+                    <span className="button__text">Editar</span>
+                  </button>
+                  <div
+                    className={`btn-load ${loading ? "button--loading" : ""}`}
+                    onClick={handlerDelete}
+                    type="submit"
+                    disabled={loading}
+                    style={{
+                      marginTop: "28px",
+                      padding: "9px",
+                      backgroundColor: "red",
+                      textAlign: 'center'
+                    }}
+                  >
+                    <span className="button__text">Borrar</span>
+                  </div>
+                </div>
                 {error && <p style={{ color: "red" }}>{error}</p>}
               </div>
             </Form>
